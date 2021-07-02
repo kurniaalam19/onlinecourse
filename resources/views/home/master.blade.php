8d4e92810fc0034ba408e539d56b33b5fd0397ed
kurniaalam19 <79829673+kurniaalam19@users.noreply.github.com>
2021-07-03 00:50:11 +0700

    Update master.blade.php

--- resources/views/home/master.blade.php
+++ resources/views/home/master.blade.php
@@ -24,15 +24,6 @@
             </div>
         </div>
     </nav>
-<!-- banner -->
-    
-    <div id="tm-video-container">
-        <video autoplay muted loop id="tm-video">
-            <!-- <source src="video/sunset-timelapse-video.mp4" type="video/mp4"> -->
-                <source src="https://www.youtube.com/watch?v=ew3lh7_DAeU" type="video/mp4">
-        </source>    
-    </div>
-    <br><br><br>
 
     <div class="wrapper">
         <!-- untuk home -->
